a024e54f6ca2b4b1b04dedde738d2403f73fa54f
Mahdi <mahdidvb72@gmail.com>
2019-08-23 23:33:48 +0430

    Remove Duplicate Code

--- src/Shared/HandyControl_Shared/Controls/Extra/Helper/InstanceHelper.cs
+++ src/Shared/HandyControl_Shared/Controls/Extra/Helper/InstanceHelper.cs
@@ -1,27 +1,22 @@
-﻿using System;
+﻿using HandyControl.Tools.Interop;
+using System;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace HandyControl.Controls
 {
     public class InstanceHelper
     {
-       
-        [DllImport("user32.dll")]
-        static extern bool SetForegroundWindow(IntPtr hWnd);
-
         static Mutex mutex;
 
         /// <summary>
         /// check only one instance is running 
-        /// example: IsRunOnlyOneInstance();
         /// </summary>
         /// <param name="ShowErrorMessage"></param>
         /// <param name="ErrorMessage"></param>
         /// <returns></returns>
-        public static bool IsSingleInstance(bool ShowErrorMessage = true, string ErrorMessage = "Another instance of the app is running")
+        public static bool IsSingleInstance(string ErrorMessage = "Another instance of the app is running", bool ShowErrorMessage = true)
         {
             var guid = CryptographyHelper.GenerateMD5(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
             mutex = new Mutex(true, "{" + $"{guid}" + "}");
@@ -42,14 +37,13 @@ namespace HandyControl.Controls
             }
         }
 
-        static void BringWindowToFront()
+        private static void BringWindowToFront()
         {
             var currentProcess = Process.GetCurrentProcess();
             var processes = Process.GetProcessesByName(currentProcess.ProcessName);
             var process = processes.FirstOrDefault(p => p.Id != currentProcess.Id);
             if (process == null) return;
-
-            SetForegroundWindow(process.MainWindowHandle);
+            UnsafeNativeMethods.SetForegroundWindow(process.MainWindowHandle);
         }
     }
 }
