ee6757f13f6e849f8c50c85f0d183567c9270cca
Mahdi Hosseini <mahdidvb72@gmail.com>
2019-09-14 15:39:07 +0430

    updated vsix

--- Installers/VS2019/WpfApp/MainWindow.xaml.cs
+++ Installers/VS2019/WpfApp/MainWindow.xaml.cs
@@ -1,10 +1,26 @@
-﻿namespace $safeprojectname$
+﻿using HandyControl.Data;
+using System.Windows;
+using System.Windows.Controls;
+namespace $safeprojectname$
 {
     public partial class MainWindow
+{
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    #region Change Skin
+    private void ButtonConfig_OnClick(object sender, RoutedEventArgs e) => PopupConfig.IsOpen = true;
+
+    private void ButtonSkins_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (e.OriginalSource is Button button && button.Tag is SkinType tag)
         {
-            InitializeComponent();
+            PopupConfig.IsOpen = false;
+            ((App)Application.Current).UpdateSkin(tag);
         }
     }
+    #endregion
+}
 }
--- Installers/VS2019/WpfCoreApp/MainWindow.xaml.cs
+++ Installers/VS2019/WpfCoreApp/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace $safeprojectname$
+﻿using HandyControl.Data;
+using System.Windows;
+using System.Windows.Controls;
+namespace $safeprojectname$
 {
     public partial class MainWindow
     {
@@ -6,5 +9,17 @@
         {
             InitializeComponent();
         }
+    #region Change Skin
+    private void ButtonConfig_OnClick(object sender, RoutedEventArgs e) => PopupConfig.IsOpen = true;
+
+    private void ButtonSkins_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (e.OriginalSource is Button button && button.Tag is SkinType tag)
+        {
+            PopupConfig.IsOpen = false;
+            ((App)Application.Current).UpdateSkin(tag);
+        }
     }
+    #endregion
+}
 }
