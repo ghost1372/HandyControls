15e45e36b7de1e093856e6ce713c7b9c769d546c
Mahdi <mahdidvb72@gmail.com>
2020-02-01 21:43:44 +0330

    add wpf mvvm

---
+++ Installers/VS2019/WpfAppMVVM/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using HandyControl.Data;
+namespace $safeprojectname$
+{
+    public partial class App
+{
+    internal void UpdateSkin(SkinType skin)
+    {
+        Resources.MergedDictionaries.Clear();
+        Resources.MergedDictionaries.Add(new ResourceDictionary
+        {
+            Source = new Uri($"pack://application:,,,/HandyControl;component/Themes/Skin{skin.ToString()}.xaml")
+        });
+        Resources.MergedDictionaries.Add(new ResourceDictionary
+        {
+            Source = new Uri("pack://application:,,,/HandyControl;component/Themes/Theme.xaml")
+        });
+    }
+}
+}
---
+++ Installers/VS2019/WpfAppMVVM/ViewModelLocator.cs
@@ -0,0 +1,28 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Windows;
+namespace $safeprojectname$.ViewModels
+{
+    public class ViewModelLocator
+{
+    public ViewModelLocator()
+    {
+        ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+        SimpleIoc.Default.Register<MainViewModel>();
+
+        // Register ViewModels Here
+
+    }
+
+    public static ViewModelLocator Instance => new Lazy<ViewModelLocator>(() =>
+        Application.Current.TryFindResource("Locator") as ViewModelLocator).Value;
+
+    #region Vm
+
+    public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+
+    #endregion
+}
+}
