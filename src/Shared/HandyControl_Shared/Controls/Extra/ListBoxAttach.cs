c5ce007834e1b46750f1c6fc410fc3d6cd4251e7
Mahdi <mahdidvb72@gmail.com>
2020-03-31 19:36:20 +0430

    Add IsOddEven Row AttachedProperty

---
+++ src/Shared/HandyControl_Shared/Controls/Extra/ListBoxAttach.cs
@@ -0,0 +1,18 @@
+﻿using HandyControl.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace HandyControl.Controls
+{
+    public class ListBoxAttach
+    {
+        public static readonly DependencyProperty IsOddEvenRowProperty = DependencyProperty.RegisterAttached(
+           "IsOddEvenRow", typeof(bool), typeof(ListBoxAttach), new FrameworkPropertyMetadata(ValueBoxes.FalseBox, FrameworkPropertyMetadataOptions.Inherits));
+
+        public static void SetIsOddEvenRow(DependencyObject element, bool value) => element.SetValue(IsOddEvenRowProperty, value);
+
+        public static bool GetIsOddEvenRowProperty(DependencyObject element) => (bool)element.GetValue(IsOddEvenRowProperty);
+    }
+}
