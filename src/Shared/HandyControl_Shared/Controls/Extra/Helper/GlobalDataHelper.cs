470d17948190918af8509acddd486c48853c508e
Mahdi <mahdidvb72@gmail.com>
2020-04-16 15:15:38 +0430

    add GlobalDataHelper for .NetCore > 3.0

---
+++ src/Shared/HandyControl_Shared/Controls/Extra/Helper/GlobalDataHelper.cs
@@ -0,0 +1,43 @@
+﻿#if Core
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace HandyControl.Controls
+{
+    public abstract class GlobalDataHelper<T> where T : GlobalDataHelper<T>, new()
+    {
+        public static T Config { get; set; }
+        private static string _filename { get; set; }
+
+        /// <summary>
+        /// This function will load settings object.
+        /// </summary>
+        /// <param name="FileName">config file location with filename</param>
+        public static void Init(string FileName = "AppConfig.json")
+        {
+            _filename = FileName;
+            if (File.Exists(FileName))
+            {
+                string json = File.ReadAllText(FileName);
+
+                Config = (string.IsNullOrEmpty(json) ? new T() : JsonSerializer.Deserialize<T>(json)) ?? new T();
+            }
+            else
+            {
+                Config = new T();
+            }
+        }
+
+        public static void Save()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true, IgnoreNullValues = true };
+
+            string json = JsonSerializer.Serialize(Config, options);
+            File.WriteAllText(_filename, json);
+        }
+    }
+}
+#endif
