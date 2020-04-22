5cc170201c8e0c40ba57260d10d23b0fcffa0471
Mahdi <mahdidvb72@gmail.com>
2020-04-22 20:09:28 +0430

    update vsix

--- Installers/VS2019/WpfAppMVVMPrism/App.xaml.cs
+++ Installers/VS2019/WpfAppMVVMPrism/App.xaml.cs
@@ -18,15 +18,8 @@ namespace $safeprojectname$
     }
     internal void UpdateSkin(SkinType skin)
     {
-        Resources.MergedDictionaries.Clear();
-        Resources.MergedDictionaries.Add(new ResourceDictionary
-        {
-            Source = new Uri($"pack://application:,,,/HandyControl;component/Themes/Skin{skin.ToString()}.xaml")
-        });
-        Resources.MergedDictionaries.Add(new ResourceDictionary
-        {
-            Source = new Uri("pack://application:,,,/HandyControl;component/Themes/Theme.xaml")
-        });
+        ResourceHelper.GetTheme("HandyTheme", Resources).Skin = skin;
+        Current.MainWindow?.OnApplyTemplate();
     }
 }
 }
