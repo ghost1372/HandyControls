a4b866205dab70effade56350988d653cc28cd70
Mahdi <mahdidvb72@gmail.com>
2020-05-23 22:23:52 +0430

    add Extensions

---
+++ src/Shared/HandyControl_Shared/Controls/Extra/Extension/ConvertTypes.cs
@@ -0,0 +1,38 @@
+﻿namespace HandyControl.Tools.Extension
+{
+    public static class ConvertTypes
+    {
+        /// <summary>
+        /// Converts any type in to an Int32
+        /// </summary>
+        /// <typeparam name="T">Any Object</typeparam>
+        /// <param name="value">Value to convert</param>
+        /// <returns>The integer, 0 if unsuccessful</returns>
+        public static int ToInt32<T>(this T value)
+        {
+            int result;
+            if (int.TryParse(value.ToString(), out result))
+            {
+                return result;
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Converts any type in to an Int32 but if null then returns the default
+        /// </summary>
+        /// <param name="value">Value to convert</param>
+        /// <typeparam name="T">Any Object</typeparam>
+        /// <param name="defaultValue">Default to use</param>
+        /// <returns>The defaultValue if unsuccessful</returns>
+        public static int ToInt32<T>(this T value, int defaultValue)
+        {
+            int result;
+            if (int.TryParse(value.ToString(), out result))
+            {
+                return result;
+            }
+            return defaultValue;
+        }
+    }
+}
---
+++ src/Shared/HandyControl_Shared/Controls/Extra/Extension/InlineFormatString.cs
@@ -0,0 +1,16 @@
+﻿namespace HandyControl.Tools.Extension
+{
+    public static class InlineFormatString
+    {
+        /// <summary>
+        /// Enable quick and more natural string.Format calls
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static string Format(this string input, params object[] args)
+        {
+            return string.Format(input, args);
+        }
+    }
+}
