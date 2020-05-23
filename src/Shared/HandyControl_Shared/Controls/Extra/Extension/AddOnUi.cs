9f64310c7dd0c99fb52276ce4ba7866597f59719
Mahdi <mahdidvb72@gmail.com>
2020-05-23 21:58:14 +0430

    add Some Extension

---
+++ src/Shared/HandyControl_Shared/Controls/Extra/Extension/AddOnUi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+namespace HandyControl.Tools.Extension
+{
+    public static class AddOnUi
+    {
+        /// <summary>
+        /// This Extension Method Help you to Add Items into ObservableCollection from Another Thread
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="item"></param>
+        public static void AddOnUI<T>(this ICollection<T> collection, T item)
+        {
+            Action<T> addMethod = collection.Add;
+            Application.Current.Dispatcher.BeginInvoke(addMethod, item);
+        }
+    }
+}
