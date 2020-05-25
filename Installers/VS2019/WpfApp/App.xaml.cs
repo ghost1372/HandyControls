b17279612918f30d1d7f47364d37258c326b43c8
Mahdi <mahdidvb72@gmail.com>
2020-05-25 15:56:36 +0430

    update vsix

--- Installers/VS2019/WpfApp/App.xaml.cs
+++ Installers/VS2019/WpfApp/App.xaml.cs
@@ -1,13 +1,21 @@
 ﻿using HandyControl.Data;
+using HandyControl.Themes;
 using HandyControl.Tools;
+using System.Windows;
+using System;
 namespace $safeprojectname$
 {
     public partial class App
     {
         internal void UpdateSkin(SkinType skin)
         {
-            ResourceHelper.GetTheme("HandyTheme", Resources).Skin = skin;
+            SharedResourceDictionary.SharedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(ResourceHelper.GetSkin(skin));
+            Resources.MergedDictionaries.Add(new ResourceDictionary
+            {
+                Source = new Uri("pack://application:,,,/HandyControl;component/Themes/Theme.xaml")
+            });
             Current.MainWindow?.OnApplyTemplate();
         }
-}
+    }
 }
--- Installers/VS2019/WpfAppMVVMPrism/App.xaml.cs
+++ Installers/VS2019/WpfAppMVVMPrism/App.xaml.cs
@@ -1,26 +1,32 @@
-﻿using System;
-using System.Windows;
-using HandyControl.Data;
+﻿using HandyControl.Data;
+using HandyControl.Themes;
 using HandyControl.Tools;
-using $safeprojectname$.Views;
+using System.Windows;
+using System;
 using Prism.Ioc;
+using $safeprojectname$.Views;
 namespace $safeprojectname$
 {
     public partial class App
-{
-    protected override Window CreateShell()
     {
-        return Container.Resolve<MainWindow>();
-    }
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
 
-    protected override void RegisterTypes(IContainerRegistry containerRegistry)
-    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
 
+        }
+        internal void UpdateSkin(SkinType skin)
+        {
+            SharedResourceDictionary.SharedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(ResourceHelper.GetSkin(skin));
+            Resources.MergedDictionaries.Add(new ResourceDictionary
+            {
+                Source = new Uri("pack://application:,,,/HandyControl;component/Themes/Theme.xaml")
+            });
+            Current.MainWindow?.OnApplyTemplate();
+        }
     }
-    internal void UpdateSkin(SkinType skin)
-    {
-        ResourceHelper.GetTheme("HandyTheme", Resources).Skin = skin;
-        Current.MainWindow?.OnApplyTemplate();
-    }
-}
 }
