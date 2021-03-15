394348e23a68cd09719f49546f0b5f5398778769
Mahdi <mahdidvb72@gmail.com>
2021-03-15 22:02:51 +0330

    refactor UpdateHelper

--- src/Shared/HandyControl_Shared/HandyControls/Tools/Helper/UpdateHelper.cs
+++ src/Shared/HandyControl_Shared/HandyControls/Tools/Helper/UpdateHelper.cs
@@ -1,30 +1,28 @@
 ﻿#if !NET40
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Reflection;
 using System.Net.Http;
+using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 #if NETCOREAPP
 using System.Text.Json;
 #else
 using System.Web.Script.Serialization;
 #endif
-using System.Threading.Tasks;
 using HandyControl.Data;
+using HandyControl.Tools.Extension;
 
 namespace HandyControl.Tools
 {
     public class UpdateHelper
     {
-        public static UpdateHelper Instance => new UpdateHelper();
-
-        private SystemVersionInfo GetSystemVersion(string version)
+        private const string GITHUB_API = "https://api.github.com/repos/{0}/{1}/releases/latest";
+        private static SystemVersionInfo GetAsVersionInfo(string version)
         {
-            string removedchar = RemoveExtraText(version);
-            var nums = removedchar.Split('.').Select(int.Parse).ToList();
+            var nums = GetVersionNumbers(version).Split('.').Select(int.Parse).ToList();
 
             if (nums.Count <= 3)
             {
@@ -34,137 +32,102 @@ namespace HandyControl.Tools
             return new SystemVersionInfo(nums[0], nums[1], nums[2], nums[3]);
         }
 
-        private string RemoveExtraText(string version)
+        private static string GetVersionNumbers(string version)
         {
             var allowedChars = "01234567890.";
             return new string(version.Where(c => allowedChars.Contains(c)).ToArray());
         }
 
-        public async Task<GithubModel> CheckUpdateAsync(string username, string repository)
+        public static ReleaseInfo CheckUpdate(string username, string repository)
         {
-            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(repository))
+            if (string.IsNullOrEmpty(username))
+                throw new ArgumentNullException(nameof(username));
+
+            if (string.IsNullOrEmpty(repository))
+                throw new ArgumentNullException(nameof(repository));
+
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType) 3072;
+            string url = GITHUB_API.Format(username, repository);
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+            request.UserAgent = username;
+            WebResponse response = request.GetResponse();
+            using (Stream responseStream = response.GetResponseStream())
             {
-                ServicePointManager.SecurityProtocol = (SecurityProtocolType) 3072;
-                HttpClient client = new HttpClient();
-                client.DefaultRequestHeaders.Add("User-Agent", username);
-                string url = $"https://api.github.com/repos/{username}/{repository}/releases/latest";
-                HttpResponseMessage response = await client.GetAsync(url);
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
 #if NETCOREAPP
-                var result = JsonSerializer.Deserialize<RootModel>(responseBody);
+                var result = System.Text.Json.JsonSerializer.Deserialize<ReleaseInfo>(reader.ReadToEnd());
 #else
                 JavaScriptSerializer javaScript = new JavaScriptSerializer();
-                var result = javaScript.Deserialize<RootModel>(responseBody);
+                javaScript.RegisterConverters(new JavaScriptConverter[] { new DataContractJavaScriptConverter(true) });
+
+                var result = javaScript.Deserialize<ReleaseInfo>(reader.ReadToEnd());
 #endif
                 if (result != null)
                 {
-                    var newInfo = GetSystemVersion(result.tag_name);
-                    var oldInfo = GetSystemVersion(Assembly.GetCallingAssembly().GetName().Version?.ToString());
-                    var newVer = new SystemVersionInfo(newInfo.Major, newInfo.Minor, newInfo.Build, newInfo.Revision);
-                    var oldVer = new SystemVersionInfo(oldInfo.Major, oldInfo.Minor, oldInfo.Build, oldInfo.Revision);
+                    var newVersion = GetAsVersionInfo(result.TagName);
+                    var oldVersion = GetAsVersionInfo(Assembly.GetEntryAssembly().GetName().Version?.ToString());
 
-                    var model = new GithubModel
+                    return new ReleaseInfo
                     {
-                        Changelog = result?.body,
-                        CreatedAt = Convert.ToDateTime(result?.created_at),
-                        Asset = result.assets,
-                        IsPreRelease = result.prerelease,
-                        PublishedAt = Convert.ToDateTime(result?.published_at),
-                        TagName = result.tag_name,
-                        ApiUrl = result?.url,
-                        ReleaseUrl = result?.html_url,
-                        IsExistNewVersion = newVer > oldVer
+                        Changelog = result?.Changelog,
+                        CreatedAt = Convert.ToDateTime(result?.CreatedAt),
+                        Assets = result.Assets,
+                        IsPreRelease = result.IsPreRelease,
+                        PublishedAt = Convert.ToDateTime(result?.PublishedAt),
+                        TagName = result.TagName,
+                        ApiUrl = result?.ApiUrl,
+                        ReleaseUrl = result?.ReleaseUrl,
+                        IsExistNewVersion = newVersion > oldVersion
                     };
-
-
-                    return model;
                 }
             }
-            else
-            {
-                throw new Exception("Username and Repository can not be empty.");
-            }
 
-            return new GithubModel();
+            return new ReleaseInfo();
         }
 
-        public GithubModel CheckUpdate(string username, string repository)
+        public static async Task<ReleaseInfo> CheckUpdateAsync(string username, string repository)
         {
-            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(repository))
-            {
-                ServicePointManager.SecurityProtocol = (SecurityProtocolType) 3072;
-                string url = $"https://api.github.com/repos/{username}/{repository}/releases/latest";
-                HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
-                request.UserAgent = username;
-                WebResponse response = request.GetResponse();
-                using (Stream responseStream = response.GetResponseStream())
-                {
-                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+            if (string.IsNullOrEmpty(username))
+                throw new ArgumentNullException(nameof(username));
+
+            if (string.IsNullOrEmpty(repository))
+                throw new ArgumentNullException(nameof(repository));
+
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType) 3072;
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", username);
+            string url = GITHUB_API.Format(username, repository);
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
 #if NETCOREAPP
-                    var result = System.Text.Json.JsonSerializer.Deserialize<RootModel>(reader.ReadToEnd());
+            var result = JsonSerializer.Deserialize<ReleaseInfo>(responseBody);
 #else
-                    JavaScriptSerializer javaScript = new JavaScriptSerializer();
-                    var result = javaScript.Deserialize<RootModel>(reader.ReadToEnd());
+            JavaScriptSerializer javaScript = new JavaScriptSerializer();
+            javaScript.RegisterConverters(new JavaScriptConverter[] { new DataContractJavaScriptConverter(true) });
+
+            var result = javaScript.Deserialize<ReleaseInfo>(responseBody);
 #endif
-                    if (result != null)
-                    {
-                        var newInfo = GetSystemVersion(result.tag_name);
-                        var oldInfo = GetSystemVersion(Assembly.GetCallingAssembly().GetName().Version?.ToString());
-                        var newVer = new SystemVersionInfo(newInfo.Major, newInfo.Minor, newInfo.Build, newInfo.Revision);
-                        var oldVer = new SystemVersionInfo(oldInfo.Major, oldInfo.Minor, oldInfo.Build, oldInfo.Revision);
-
-                        var model = new GithubModel
-                        {
-                            Changelog = result?.body,
-                            CreatedAt = Convert.ToDateTime(result?.created_at),
-                            Asset = result.assets,
-                            IsPreRelease = result.prerelease,
-                            PublishedAt = Convert.ToDateTime(result?.published_at),
-                            TagName = result.tag_name,
-                            ApiUrl = result?.url,
-                            ReleaseUrl = result?.html_url,
-                            IsExistNewVersion = newVer > oldVer
-                        };
-
-                        return model;
-                    }
-                }
-            }
-            else
+            if (result != null)
             {
-                throw new Exception("Username and Repository can not be empty.");
+                var newVersion = GetAsVersionInfo(result.TagName);
+                var oldVersion = GetAsVersionInfo(Assembly.GetEntryAssembly().GetName().Version?.ToString());
+
+                return new ReleaseInfo
+                {
+                    Changelog = result?.Changelog,
+                    CreatedAt = Convert.ToDateTime(result?.CreatedAt),
+                    Assets = result.Assets,
+                    IsPreRelease = result.IsPreRelease,
+                    PublishedAt = Convert.ToDateTime(result?.PublishedAt),
+                    TagName = result.TagName,
+                    ApiUrl = result?.ApiUrl,
+                    ReleaseUrl = result?.ReleaseUrl,
+                    IsExistNewVersion = newVersion > oldVersion
+                };
             }
 
-            return new GithubModel();
-        }
-        public class Asset
-        {
-            public int size { get; set; }
-            public string browser_download_url { get; set; }
-        }
-        private class RootModel
-        {
-            public string url { get; set; }
-            public string html_url { get; set; }
-            public string tag_name { get; set; }
-            public bool prerelease { get; set; }
-            public DateTime created_at { get; set; }
-            public DateTime published_at { get; set; }
-            public List<Asset> assets { get; set; }
-            public string body { get; set; }
-        }
-        public class GithubModel
-        {
-            public bool IsExistNewVersion { get; internal set; }
-            public string ApiUrl { get; internal set; }
-            public string ReleaseUrl { get; internal set; }
-            public string Changelog { get; internal set; }
-            public string TagName { get; internal set; }
-            public List<Asset> Asset { get; internal set; }
-            public bool IsPreRelease { get; internal set; }
-            public DateTime CreatedAt { get; internal set; }
-            public DateTime PublishedAt { get; internal set; }
+            return new ReleaseInfo();
         }
     }
 }
