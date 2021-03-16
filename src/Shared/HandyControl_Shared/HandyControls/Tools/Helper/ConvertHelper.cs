ea63c9e1c25cabc3606dee4cd0cc02027d1eddd4
Mahdi <mahdidvb72@gmail.com>
2021-03-16 17:33:01 +0330

    Add ConvertHelper

---
+++ src/Shared/HandyControl_Shared/HandyControls/Tools/Helper/ConvertHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace HandyControl.Tools
+{
+    public class ConvertHelper
+    {
+        public static BitmapImage BytesToBitmapImage(byte[] bytes)
+        {
+            MemoryStream stream = new MemoryStream(bytes);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = stream;
+            image.EndInit();
+            return image;
+        }
+
+        public static byte[] BitmapImageToBytes(BitmapImage bitmapImage)
+        {
+            MemoryStream memStream = new MemoryStream();
+            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bitmapImage));
+            encoder.Save(memStream);
+            return memStream.ToArray();
+        }
+
+        public static string BitmapImageToBase64(BitmapImage bitmapImage)
+        {
+            return Convert.ToBase64String(BitmapImageToBytes(bitmapImage));
+        }
+
+        public static BitmapImage Base64ToBitmapImage(string base64)
+        {
+            byte[] binaryData = Convert.FromBase64String(base64);
+            return BytesToBitmapImage(binaryData);
+        }
+    }
+}
