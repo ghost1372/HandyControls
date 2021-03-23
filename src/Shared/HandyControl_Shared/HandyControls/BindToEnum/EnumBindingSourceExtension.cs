e5d6a48dc41c6808ff50186d9aa14042dc6a9856
Mahdi <mahdidvb72@gmail.com>
2021-03-23 14:40:32 +0430

    Add Description property in EnumBindingSourceExtension for built-in or 3rd party enums
    now you can add description for enums that you dont have access

--- src/Shared/HandyControl_Shared/HandyControls/BindToEnum/EnumBindingSourceExtension.cs
+++ src/Shared/HandyControl_Shared/HandyControls/BindToEnum/EnumBindingSourceExtension.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Markup;
 
 namespace HandyControl.Controls
 {
     public class EnumBindingSourceExtension : MarkupExtension
     {
+        bool _isDescription = false;
         private Type _enumType;
+        private string[] _description;
 
         public EnumBindingSourceExtension() { }
 
         public EnumBindingSourceExtension(Type enumType)
         {
             EnumType = enumType;
+            _isDescription = false;
+        }
+
+        public EnumBindingSourceExtension(Type enumType, string[] description)
+        {
+            EnumType = enumType;
+            Description = description;
+            _isDescription = true;
         }
 
         public Type EnumType
@@ -36,6 +47,18 @@ namespace HandyControl.Controls
             }
         }
 
+        public string[] Description
+        {
+            get => _description;
+            set
+            {
+                if (value != _description)
+                {
+                    _description = value;
+                }
+            }
+        }
+
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
             if (null == _enumType)
@@ -46,14 +69,43 @@ namespace HandyControl.Controls
             Type actualEnumType = Nullable.GetUnderlyingType(_enumType) ?? _enumType;
             Array enumValues = Enum.GetValues(actualEnumType);
 
-            if (actualEnumType == _enumType)
+
+            if (_isDescription)
             {
-                return enumValues;
+                Dictionary<object, string> dic = new Dictionary<object, string>();
+                int index = 0;
+                foreach (var item in enumValues)
+                {
+                    if (index < Description.Length)
+                    {
+                        dic.Add(item, Description[index]);
+                    }
+                    else
+                    {
+                        dic.Add(item, item.ToString());
+
+                    }
+                    index += 1;
+                }
+
+                if (actualEnumType == _enumType)
+                {
+                    return dic;
+                }
+            }
+            else
+            {
+                if (actualEnumType == _enumType)
+                {
+                    return enumValues;
+                }
+
+                Array tempArray = Array.CreateInstance(actualEnumType, enumValues.Length + 1);
+                enumValues.CopyTo(tempArray, 1);
+                return tempArray;
             }
 
-            Array tempArray = Array.CreateInstance(actualEnumType, enumValues.Length + 1);
-            enumValues.CopyTo(tempArray, 1);
-            return tempArray;
+            return null;
         }
     }
 }
