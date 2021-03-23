3c0c2104e9b65bd10f150bed6a1e90334ece6b9f
Mahdi <mahdidvb72@gmail.com>
2021-03-23 19:06:04 +0430

    update vsix

--- Installers/VS2019/HandyControl_VS2019/Properties/AssemblyInfo.cs
+++ Installers/VS2019/HandyControl_VS2019/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("3.2.0.0")]
-[assembly: AssemblyFileVersion("3.2.0.0")]
+[assembly: AssemblyVersion("3.3.0.0")]
+[assembly: AssemblyFileVersion("3.3.0.0")]
