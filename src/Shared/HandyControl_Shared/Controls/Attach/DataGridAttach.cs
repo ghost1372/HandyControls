914d190262913473a82516ba5b55de961b5bc0a6
Mahdi <mahdidvb72@gmail.com>
2021-04-13 22:16:56 +0430

    Add DataGrid UnSelectAll Behavior

--- src/Shared/HandyControl_Shared/Controls/Attach/DataGridAttach.cs
+++ src/Shared/HandyControl_Shared/Controls/Attach/DataGridAttach.cs
@@ -341,5 +341,52 @@ namespace HandyControl.Controls
 
         public static bool GetCanUnselectAllWithBlankArea(DependencyObject element)
             => (bool) element.GetValue(CanUnselectAllWithBlankAreaProperty);
+
+        #region HandyControls
+        internal static readonly DependencyProperty IsSelectAllProperty =
+            DependencyProperty.RegisterAttached("IsSelectAll", typeof(bool), typeof(DataGridAttach), new PropertyMetadata(false,
+                (o, e) =>
+                {
+                    var dg = GetCurrentDataGrid(o);
+                    ToggleButton tg = o as ToggleButton;
+
+                    if (dg.SelectedItems.Count < 2)
+                    {
+                        tg.IsChecked = true;
+                    }
+
+                    if (tg.IsChecked == true)
+                    {
+                        dg.SelectAll();
+                    }
+                    else
+                    {
+                        dg.UnselectAll();
+                    }
+
+                }));
+        internal static bool GetIsSelectAll(DependencyObject obj)
+        {
+            return (bool) obj.GetValue(IsSelectAllProperty);
+        }
+
+        internal static void SetIsSelectAll(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsSelectAllProperty, value);
+        }
+
+        internal static readonly DependencyProperty CurrentDataGridProperty =
+            DependencyProperty.RegisterAttached("CurrentDataGrid", typeof(DataGrid), typeof(DataGridAttach), new PropertyMetadata(null));
+
+        internal static DataGrid GetCurrentDataGrid(DependencyObject obj)
+        {
+            return (DataGrid) obj.GetValue(CurrentDataGridProperty);
+        }
+
+        internal static void SetCurrentDataGrid(DependencyObject obj, DataGrid value)
+        {
+            obj.SetValue(CurrentDataGridProperty, value);
+        }
+        #endregion
     }
 }
