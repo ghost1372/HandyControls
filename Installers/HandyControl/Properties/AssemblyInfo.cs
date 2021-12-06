5d3e379e3fc022b2f354eff1eff5ed293ac208de
Mahdi Hosseini <mahdidvb72@gmail.com>
2021-12-06 11:21:26 +0330

    Update VSIX to v3.3.9

--- Installers/HandyControl/Properties/AssemblyInfo.cs
+++ Installers/HandyControl/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.8.0")]
-[assembly: AssemblyFileVersion("3.3.8.0")]
+[assembly: AssemblyVersion("3.3.9.0")]
+[assembly: AssemblyFileVersion("3.3.9.0")]
